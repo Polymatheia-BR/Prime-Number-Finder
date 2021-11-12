d870cf4eb2929d7c994581d4323a8e93541f43fd
Victor Ferreira <vffvmode@gmail.com>
2021-11-12 18:35:05 -0300

    Programa exemplo feito

--- src/Program.java
+++ src/Program.java
@@ -2,7 +2,22 @@
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int z = 0;
+		
+		for(int x = 1;x<=100;x++) {
+			for(int i = 1;i<=x;i++) {
+				if(x%i == 0) {
+					z++;
+				}
+			}
+			if(z == 2) {
+				System.out.println(x);
+				z = 0;
+			} else {
+				z = 0;
+			}
+			
+		}
 
 	}
 
